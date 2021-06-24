2f64b75df11d44459fa97df141af17e163c4c45e
Megan Leich <51799170+maleich@users.noreply.github.com>
2021-06-23 20:46:08 -0700

    Initial commit

---
+++ tests/test_exercise.py
@@ -0,0 +1,29 @@
+import pytest
+import src.exercise
+
+inp_1 = []
+out_1 = []
+
+inp_2 = []
+out_2 = []
+
+# run the test function for each input/output pair
+@pytest.mark.parametrize("test_input, expected", [(inp_1, out_1), (inp_2, out_2)])
+def test_capture_stdout(capsys, test_input, expected):
+    
+    # Load the test input for the program execution:
+    def mock_input(s):
+        return test_input.pop(0)
+    src.exercise.input = mock_input
+    
+    # Execute the student program, and capture the output (print statements):
+    src.exercise.main()
+    out, err = capsys.readouterr()
+
+    # Reformat program output as a list of strings.
+    # Each line of output will be a list element, excluding blank newlines.
+    out = out.strip().split('\n')
+    out = [i for i in out if i]
+
+    # Test the actual program output against the anticipated program output:
+    assert out == expected
