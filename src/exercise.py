a64ed4eb5529e6bc677dd6de24fbe8a82ec8d8d7
mleich <mleich@athenian.org>
2021-06-23 21:10:35 -0700

    test autograding

--- src/exercise.py
+++ src/exercise.py
@@ -2,6 +2,7 @@ def main(x):
     return x*2
 
 
+
 if __name__ == '__main__': 
     main(5)
 
--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,25 +1,8 @@
-import pytest
+
 import src.exercise
 
-inp_1 = [5]
-out_1 = [10]
 
-
-def test_capture_stdout(capsys):
-    # Load the test input for the program execution:
-    def mock_input(s):
-        return inp_1.pop(0)
-
-    src.exercise.input = mock_input
-
-    # Execute the student program, and capture the output (print statements):
-    src.exercise.main(5)
-    out, err = capsys.readouterr()
-
-    # Reformat program output as a list of strings.
-    # Each line of output will be a list element, excluding blank newlines.
-    out = out.strip().split('\n')
-    out = [i for i in out if i]
-
-    # Test the actual program output against the anticipated program output:
-    assert out == out_1
+def test_double_it():
+    assert 8 == src.exercise.main(4)
+    assert 0 == src.exercise.main(0)
+    assert -2 == src.exercise.main(-1)
